3ea9f2f87e469e727b0443c4fe4282205fbcd241
Harish Karumuthil <harish2704@gmail.com>
2025-06-03 02:54:25 +0530

    Revert "Fix num_workers warning"
    This reverts commit c62285db7376a384052ef413c0494c7f80a24587.

--- src/python/piper_train/vits/lightning.py
+++ src/python/piper_train/vits/lightning.py
@@ -179,7 +179,7 @@ class VitsModel(pl.LightningModule):
                 is_multispeaker=self.hparams.num_speakers > 1,
                 segment_size=self.hparams.segment_size,
             ),
-            num_workers=11,
+            num_workers=self.hparams.num_workers,
             batch_size=self.hparams.batch_size,
         )
 
